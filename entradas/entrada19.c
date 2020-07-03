c9965665de202ac50169b1626ad1ce181898e8cc
gpeitzner <3000809800101@ingenieria.usac.edu.gt>
2020-07-02 22:57:46 -0600

    Ejecutable generado

---
+++ entradas/entrada19.c
@@ -0,0 +1,25 @@
+//entrada19.c
+
+int main(){
+	int var1 = 1;
+	int var2 = 2;
+	var1 = var2;
+	var2 = var1;
+	var1 += 0;
+	var2 -= 0;
+	var1 *= 1;
+	var2 /= 1;
+	var1 = 1;
+	var2 = 2;
+	var1 = var2 + 0;
+	var2 = var1 - 0;
+	var1 = var2 * 1;
+	var2 = var1 / 1;
+	var1 = 1;
+	var2 = 2;
+	var1 = var2 * 2;
+	var2 = var1 * 0;
+	var1 = 1;
+	var2 = 2;
+	var1 = 0 / var2;
+}
